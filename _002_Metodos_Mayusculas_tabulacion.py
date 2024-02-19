83b728a6ae05e2c2f0e44f437ae695be6ad2a51e
salvador Meza <77693692+salvador0401@users.noreply.github.com>
2024-02-18 23:32:24 -0600

    Add files via upload

---
+++ _002_Metodos_Mayusculas_tabulacion.py
@@ -0,0 +1,13 @@
+##________  ___  ___  ________  ___      ___ ________     
+##|\   ____\|\  \|\  \|\   __  \|\  \    /  /|\   __  \    
+##\ \  \___|\ \  \\\  \ \  \|\  \ \  \  /  / | \  \|\  \   
+## \ \  \    \ \   __  \ \   __  \ \  \/  / / \ \   __  \  
+##  \ \  \____\ \  \ \  \ \  \ \  \ \    / /   \ \  \ \  \ 
+##   \ \_______\ \__\ \__\ \__\ \__\ \__/ /     \ \__\ \__\
+##    \|_______|\|__|\|__|\|__|\|__|\|__|/       \|__|\|__|
+##21310195 Meza Morales Salvador Emmanuel
+a='aDIOS pUNTO y cOMA'#se guarda un string con un texto en mayusculas y minusculas
+print('Original\n\t',a)#Se imprime el string y a la vez se usa un salto de linea(\n) y una tabulacion (\t)
+print('Con Upper\n\t',a.upper())#Upper sirve para poner en mayusculas todas las letras del string
+print('Con Title\n\t',a.title())#Title pone la letra inicial de cada oracion en mayusculas
+print('Con lower\n\t',a.lower())#Lower convierte todas las letra del string en minusculas
