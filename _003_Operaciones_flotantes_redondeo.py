bb953193870e21695bf22b7c55115b1a767cba6f
salvador Meza <77693692+salvador0401@users.noreply.github.com>
2024-02-19 23:11:23 -0600

    Add files via upload

---
+++ _003_Operaciones_flotantes_redondeo.py
@@ -0,0 +1,22 @@
+##________  ___  ___  ________  ___      ___ ________     
+##|\   ____\|\  \|\  \|\   __  \|\  \    /  /|\   __  \    
+##\ \  \___|\ \  \\\  \ \  \|\  \ \  \  /  / | \  \|\  \   
+## \ \  \    \ \   __  \ \   __  \ \  \/  / / \ \   __  \  
+##  \ \  \____\ \  \ \  \ \  \ \  \ \    / /   \ \  \ \  \ 
+##   \ \_______\ \__\ \__\ \__\ \__\ \__/ /     \ \__\ \__\
+##    \|_______|\|__|\|__|\|__|\|__|\|__|/       \|__|\|__|
+##21310195 Meza Morales Salvador Emmanuel
+a=10.38545#creamos una variable con un valor frotante
+b=7.5455#Creamos otra variable 
+suma=a+b#sumamos las variables
+resta=a-b#restamos las variabbles
+multiplicacion=a*b#multiplicamos las varaibles
+division=a/b#dividimos las variables
+exponencial=a**b#exponenciamos las variables
+print('El primer numero es: ',a,'el segundo es: ',b)#imprimimos las variables que pusimos
+print('La suma da: \n',suma)#imprimimos el resultados de la suma
+print('La resta da: \n',resta)#imprimimos el resultados de la resta
+print('La multiplicacion da: \n',multiplicacion)#imprimimos el resultados de la multiplicacion
+print('La division da: \n',division)#imprimimos el resultados de la division
+print('El exponencial es: \n',exponencial)#imprimimos el resultados de la exponencial
+print('Redondeando el exponencial a 2 cifras queda: \n',round(exponencial,2))#imprimimos el resultados de la exponencial redondeada
