5f0a6a6cf3bb81101b1974ae63d3b5b7ff50440e
salvador Meza <77693692+salvador0401@users.noreply.github.com>
2024-02-20 22:53:24 -0600

    Update _007_If_listas.py

--- _007_If_listas.py
+++ _007_If_listas.py
@@ -6,9 +6,9 @@
 ##   \ \_______\ \__\ \__\ \__\ \__\ \__/ /     \ \__\ \__\
 ##    \|_______|\|__|\|__|\|__|\|__|\|__|/       \|__|\|__|
 ##21310195 Meza Morales Salvador Emmanuel
-sorteo = [1,7,0]
-eleccion =int(input('Elige un numero del 0-9 y te dire si ganaste o no\n'))
-if eleccion in sorteo:
-    print('Que suerte te ganaste unos besotes')
-else:
-    print('No ganaste pero como premio de consolacion te dare unos besotes')
+sorteo = [1,7,0]#Creamos un array con 3 elementos tipo int
+eleccion =int(input('Elige un numero del 0-9 y te dire si ganaste o no\n'))#preguntamos un numero del 0 al 9 y lo guardamos en la variable eleccion 
+if eleccion in sorteo:#comprobamos si algun valor es el mismo que eligio el usuario
+    print('Que suerte te ganaste unos besotes')#si gana se dira esto
+else:#si no esta en nuestra lista se entra a la siguiente tabulacion
+    print('No ganaste pero como premio de consolacion te dare unos besotes')#Si no gan se dira esto
