1197ed4cf8679a6c2c20cc8997c7c3c1b0205300
salvador Meza <77693692+salvador0401@users.noreply.github.com>
2024-02-20 22:31:33 -0600

    Add files via upload

---
+++ _006_If_else_elif.py
@@ -0,0 +1,17 @@
+##________  ___  ___  ________  ___      ___ ________     
+##|\   ____\|\  \|\  \|\   __  \|\  \    /  /|\   __  \    
+##\ \  \___|\ \  \\\  \ \  \|\  \ \  \  /  / | \  \|\  \   
+## \ \  \    \ \   __  \ \   __  \ \  \/  / / \ \   __  \  
+##  \ \  \____\ \  \ \  \ \  \ \  \ \    / /   \ \  \ \  \ 
+##   \ \_______\ \__\ \__\ \__\ \__\ \__/ /     \ \__\ \__\
+##    \|_______|\|__|\|__|\|__|\|__|\|__|/       \|__|\|__|
+##21310195 Meza Morales Salvador Emmanuel
+ojos = int(input('Cuantos ojos tienes?\n'))
+if ojos < 1:
+    print('Como escribiste eso???!!')
+elif ojos > 1 and ojos < 3:
+    print('No estas tuerto')
+elif ojos < 2 and ojos > 0:
+    print('Estas tuerto')
+else:
+    print('neehh no te creo')
