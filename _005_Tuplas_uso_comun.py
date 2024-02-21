a265542f42e38a7e97f75a5d164f89e29594ba9f
salvador Meza <77693692+salvador0401@users.noreply.github.com>
2024-02-20 22:03:31 -0600

    Add files via upload

---
+++ _005_Tuplas_uso_comun.py
@@ -0,0 +1,13 @@
+##________  ___  ___  ________  ___      ___ ________     
+##|\   ____\|\  \|\  \|\   __  \|\  \    /  /|\   __  \    
+##\ \  \___|\ \  \\\  \ \  \|\  \ \  \  /  / | \  \|\  \   
+## \ \  \    \ \   __  \ \   __  \ \  \/  / / \ \   __  \  
+##  \ \  \____\ \  \ \  \ \  \ \  \ \    / /   \ \  \ \  \ 
+##   \ \_______\ \__\ \__\ \__\ \__\ \__/ /     \ \__\ \__\
+##    \|_______|\|__|\|__|\|__|\|__|\|__|/       \|__|\|__|
+##21310195 Meza Morales Salvador Emmanuel
+tuplas=(3,7,6,'Los adultos jovenes tienden a desaparecer a los: ','años')#Creamos una tupla con datos
+print('La tupla tiene estos datos:\n',tuplas)#Mostramos los datos de la tupla
+print(tuplas[3],tuplas[2]+tuplas[0]*tuplas[1],tuplas[4])#mostramos los datos de la tupla con un orden concreto y unas operaciones basicas hechas entre los elementos de la tupla
+lista=list(tuplas)#Creamos una lista con los elementos de la tupla
+print('La tupla ahora es: ',type(lista),'Con los datos: \n', lista)#Mostramos el tipo que es y los datos que tienen
