f3cfc76b1a9b7071b3c39559787a41b14989648a
salvador Meza <77693692+salvador0401@users.noreply.github.com>
2024-02-20 22:50:35 -0600

    Add files via upload

---
+++ _007_If_listas.py
@@ -0,0 +1,14 @@
+##________  ___  ___  ________  ___      ___ ________     
+##|\   ____\|\  \|\  \|\   __  \|\  \    /  /|\   __  \    
+##\ \  \___|\ \  \\\  \ \  \|\  \ \  \  /  / | \  \|\  \   
+## \ \  \    \ \   __  \ \   __  \ \  \/  / / \ \   __  \  
+##  \ \  \____\ \  \ \  \ \  \ \  \ \    / /   \ \  \ \  \ 
+##   \ \_______\ \__\ \__\ \__\ \__\ \__/ /     \ \__\ \__\
+##    \|_______|\|__|\|__|\|__|\|__|\|__|/       \|__|\|__|
+##21310195 Meza Morales Salvador Emmanuel
+sorteo = [1,7,0]
+eleccion =int(input('Elige un numero del 0-9 y te dire si ganaste o no\n'))
+if eleccion in sorteo:
+    print('Que suerte te ganaste unos besotes')
+else:
+    print('No ganaste pero como premio de consolacion te dare unos besotes')
